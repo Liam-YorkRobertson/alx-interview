c5041293112686e0684219e5df70b7ed29173dc6
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-12-22 02:50:26 +0200

    test

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
-script that reads stdin line by line and computes metrics
+log parsing
 """
+
 import sys
 import re
 
 
-def display_statistics(log):
+def output(log: dict) -> None:
     """
-    displays the stats
+    helper function to display stats
     """
     print("File size: {}".format(log["file_size"]))
     for code in sorted(log["code_frequency"]):
@@ -16,43 +17,35 @@ def display_statistics(log):
             print("{}: {}".format(code, log["code_frequency"][code]))
 
 
-def parse_logs():
-    """
-    parses logs
-    """
-    ip_address_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-    date_time_pattern = r'\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\]'
-    request_line_pattern = r'"GET /projects/260 HTTP/1.1"'
-    status_code_and_size_pattern = r'(.{3}) (\d+)'
-    regex_pattern = (
-        f'{ip_address_pattern} - '
-        f'{date_time_pattern} '
-        f'{request_line_pattern} '
-        f'{status_code_and_size_pattern}'
-    )
-    log_regex = re.compile(regex_pattern)
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
     line_count = 0
-    log = {"file_size": 0, "code_frequency":
-           {str(code): 0 for code in
-            [200, 301, 400, 401, 403, 404, 405, 500]}}
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
 
     try:
         for line in sys.stdin:
             line = line.strip()
-            match = log_regex.fullmatch(line)
-            if match:
+            match = regex.fullmatch(line)
+            if (match):
                 line_count += 1
-                code, file_size = match.group(1), int(match.group(2))
+                code = match.group(1)
+                file_size = int(match.group(2))
+
+                # File size
                 log["file_size"] += file_size
-                log["code_frequency"][code] += code.isdecimal()
-                if line_count % 10 == 0:
-                    display_statistics(log)
-    except KeyboardInterrupt:
-        pass
+
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
     finally:
-        display_statistics(log)
-
-
-if __name__ == "__main__":
-    parse_logs()
+        output(log)
+        
