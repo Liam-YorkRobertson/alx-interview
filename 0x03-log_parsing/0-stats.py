6aa1075e4dedf2bd36f736770aa63910cce0637d
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-12-22 02:40:48 +0200

    log parser

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,56 @@
 #!/usr/bin/python3
 """
-A script that reads stdin line by line and computes metrics
+script that reads stdin line by line and computes metrics
 """
 import sys
-from collections import defaultdict
+import re
 
-def process_logs():
+
+def display_statistics(log):
     """
-    function that does log parsing
+    displays the stats
     """
-    total_size = 0
-    status_counts = defaultdict(int)
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
+
+
+def parse_logs():
+    """
+    parses logs
+    """
+    ip_address_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+    date_time_pattern = r'\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\]'
+    request_line_pattern = r'"GET /projects/260 HTTP/1.1"'
+    status_code_and_size_pattern = r'(.{3}) (\d+)'
+    regex_pattern = (
+        f'{ip_address_pattern} - '
+        f'{date_time_pattern} '
+        f'{request_line_pattern} '
+        f'{status_code_and_size_pattern}'
+    )
+    log_regex = re.compile(regex_pattern)
+
+    line_count = 0
+    log = {"file_size": 0, "code_frequency":
+           {str(code): 0 for code in
+            [200, 301, 400, 401, 403, 404, 405, 500]}}
 
     try:
-        for i, line in enumerate(sys.stdin, start=1):
-            try:
-                _, _, _, _, _, status, size = line.split(maxsplit=6)
-                if "GET /projects/260 HTTP/1.1" in line:
-                    total_size += int(size)
-                    status_counts[status] += 1
+        for line in sys.stdin:
+            line = line.strip()
+            match = log_regex.fullmatch(line)
+            if match:
+                line_count += 1
+                code, file_size = match.group(1), int(match.group(2))
+                log["file_size"] += file_size
+                log["code_frequency"][code] += code.isdecimal()
+                if line_count % 10 == 0:
+                    display_statistics(log)
+    finally:
+        display_statistics(log)
 
-                if i % 10 == 0:
-                    print(f"File size: {total_size}")
-                    print('\n'.join(f"{code}: {count}" for code, count in sorted(status_counts.items())))
-
-            except (ValueError, IndexError):
-                continue
-
-    except KeyboardInterrupt:
-        pass
-
-    print(f"File size: {total_size}")
-    print('\n'.join(f"{code}: {count}" for code, count in sorted(status_counts.items())))
 
 if __name__ == "__main__":
-    process_logs()
+    parse_logs()
