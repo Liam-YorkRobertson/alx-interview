6262be21c44736088c73d918453bc767b0601b63
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-03 00:58:28 +0200

    test

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,25 +1,12 @@
 #!/usr/bin/python3
-"""
-UTF-8 validation function
-"""
+""" doc """
 
 
 def validUTF8(data):
-    """
-    determines if a given data set represents a valid UTF-8 encoding
-    """
-    bits_count = 0
-    for num in data:
-        if bits_count == 0:
-            mask = 0b10000000
-            while (num & mask) != 0:
-                bits_count += 1
-                mask >>= 1
-            if bits_count == 1 or bits_count > 4 or (num & 0b10000000) != 0:
-                return False
-            continue
-        if not (num & 0b10000000 and not (num & 0b01000000)):
-            return False
-        bits_count -= 1
-
-    return bits_count == 0
+    """ doc """
+    try:
+        maskeddata = [n & 255 for n in data]
+        bytes(maskeddata).decode("UTF-8")
+        return True
+    except Exception:
+        return False
