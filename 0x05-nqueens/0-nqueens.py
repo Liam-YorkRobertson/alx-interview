89dc2298247ce232e074c16aff3799d849e14b7b
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-10 10:29:12 +0200

    test

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,37 +5,45 @@ solves nqueens problem
 import sys
 
 
-def nqueens(n, row, board):
+def nqueens(n, y, board):
     """
-    checks if queen can be placed in the position
+    test
     """
-    if row == n:
-        print([list(pair) for pair in board])
-    else:
-        for col in range(n):
-            if all(board[i][1] != col and
-                   board[i][1] - i != col - row and
-                   board[i][1] + i != col + row
-                   for i in range(row)):
-                nqueens(n, row + 1, board + [(row, col)])
+    for x in range(n):
+        hold = 0
+        for q in board:
+            if x == q[1]:
+                hold = 1
+                break
+            if y - x == q[0] - q[1]:
+                hold = 1
+                break
+            if x - q[1] == q[0] - y:
+                hold = 1
+                break
+        if hold == 0:
+            board.append([y, x])
+            if y != n - 1:
+                nqueens(n, y + 1, board)
+            else:
+                print(board)
+            del board[-1]
 
 
 def main():
-    """
-    main function
-    """
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
     try:
         n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
+    except Exception:
+        print('N must be a number')
         sys.exit(1)
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
+
     nqueens(n, 0, [])
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
