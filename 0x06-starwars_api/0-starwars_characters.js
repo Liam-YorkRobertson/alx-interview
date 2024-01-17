e5adaa8b62d3c5c8a81a9d63e5d2537b8057b241
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-17 12:49:58 +0200

    test

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,24 +2,26 @@
 
 const request = require('request');
 
-if (process.argv.length !== 3) {
-  process.exit(1);
-}
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
+    }
+  });
+};
 
-const starWarsUrl = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
-
-request(starWarsUrl, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const filmData = JSON.parse(body);
-    const characterUrls = filmData.characters;
-
-    characterUrls.forEach((characterUrl) => {
-      request(characterUrl, (charError, charResponse, charBody) => {
-        if (!charError && charResponse.statusCode === 200) {
-          const characterData = JSON.parse(charBody);
-          console.log(characterData.name);
-        }
-      });
-    });
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
   }
-});
+);
